89f019da87fde8c0e5e40476e6331bc3e8dfac65
Diego-A-Roots <61999183+Diego-A-Roots@users.noreply.github.com>
2022-05-09 22:45:44 -0500

    Menu

--- conexion-civica-web/src/components/home/HomeScreen.js
+++ conexion-civica-web/src/components/home/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import FormNuevoReporte from './FormNuevoReporte';
+import Menu  from './Menu';
 
 const HomeScreen = () => {
 
@@ -8,6 +9,12 @@ const HomeScreen = () => {
 
   const handleShowForm = () => {
     setShowMenu( !showMenu )
+  }
+
+  const [Menuu, setMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setMenu( !Menuu )
   }
 
   return (
@@ -26,6 +33,10 @@ const HomeScreen = () => {
         {
           showMenu && <FormNuevoReporte/>
         }
+
+        {
+          Menuu && <Menu/>
+        }
         
         <Link to={'/auth/login'} className='signin-icon'>
           <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user-plus" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#003B36" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -36,14 +47,18 @@ const HomeScreen = () => {
           </svg>
         </Link>
 
-        <Link to={'#'} className='signin-menu'>
+        <button
+          id="show"
+          className='signin-menu'
+          onClick={handleShowMenu}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <line x1="4" y1="6" x2="20" y2="6" />
           <line x1="4" y1="12" x2="20" y2="12" />
           <line x1="4" y1="18" x2="20" y2="18" />
           </svg>
-        </Link>
+        </button>
         
 
 
