aecb6040c7c64a5bff3d94d39d73c0114abaaf79
Dragon Cem <DragonCem@MacBook-Pro-de-Dragon.local>
2022-05-09 22:07:49 -0500

    Hotfix HomeScreen :wrench:

--- conexion-civica-web/src/components/home/HomeScreen.js
+++ conexion-civica-web/src/components/home/HomeScreen.js
@@ -31,15 +31,6 @@ const HomeScreen = () => {
         {/* Menú desplegable para el ícono de usuario */}
 
         <NavUser />
-          
-         <Link to={'/auth/login'} className='signin-icon'>
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-plus" width="100" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#003B36" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <circle cx="9" cy="7" r="4" />
-            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-            <path d="M16 11h6m-3 -3v6" />
-          </svg>
-        </Link>
 
 
 
